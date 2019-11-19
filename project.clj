41ad57765485b4cef3e3fa707aadbcff57af2785
Isaac Johnston <isaac@superstruct.nz>
2019-11-19 13:17:11 +0800

    Migrate to day8/lein-git-inject

--- project.clj
+++ project.clj
@@ -1,36 +1,22 @@
-(defproject day8/re-frame-tracer "see :git-version below https://github.com/arrdem/lein-git-version"
+(defproject day8/re-frame-tracer "lein-git-inject/version"
   :description "Tracer for clairvoyant that is optimised for cljs-devtools"
   :url "http://github.com/day8/re-frame-tracer"
   :license {:name "MIT"}
-
-  :git-version
-  {:status-to-version
-   (fn [{:keys [tag version branch ahead ahead? dirty?] :as git-status}]
-     (if-not (string? tag)
-       ;; If git-status is nil (i.e. IntelliJ reading project.clj) then return an empty version.
-       "_"
-       (if (and (not ahead?) (not dirty?))
-         tag
-         (let [[_ major minor patch suffix] (re-find #"v?(\d+)\.(\d+)\.(\d+)(-.+)?" tag)]
-           (if (nil? major)
-             ;; If tag is poorly formatted then return GIT-TAG-INVALID
-             "GIT-TAG-INVALID"
-             (let [patch' (try (Long/parseLong patch) (catch Throwable _ 0))
-                   patch+ (inc patch')]
-               (str major "." minor "." patch+ suffix "-" ahead "-SNAPSHOT")))))))}
 
   :dependencies
   [[org.clojure/clojure "1.10.1" :scope "provided"]
    [org.clojure/clojurescript "1.10.520" :scope "provided"]
    [org.clojars.stumitchell/clairvoyant "0.2.1"]]
 
-  :plugins
-  [[me.arrdem/lein-git-version "2.0.3"]]
+  :plugins      [[day8/lein-git-inject "0.0.2"]
+                 [lein-shadow          "0.1.6"]]
 
-  :deploy-repositories [["clojars" {:sign-releases false}
-                                  :url           "https://clojars.org/repo"
-                                  :username      :env/CLOJARS_USERNAME
-                                  :password      :env/CLOJARS_PASSWORD]]
+  :middleware   [leiningen.git-inject/middleware]
+
+  :deploy-repositories [["clojars" {:sign-releases false
+                                    :url           "https://clojars.org/repo"
+                                    :username      :env/CLOJARS_USERNAME
+                                    :password      :env/CLOJARS_PASSWORD}]]
 
   :release-tasks [["deploy" "clojars"]])
 
